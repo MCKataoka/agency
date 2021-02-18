c0d6458d91a65233b1090c6ddbcfd40db882f797
MCKataoka <mario.c.kataoka@gmail.com>
2021-02-18 17:03:59 +0000

    adding models

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export DATABASE_URL='postgres://fdtixnzpqczohg:4b0218613153bd586c7c757a7d85eee7b258203a9893e517679faefc915e8c06@ec2-34-239-33-57.compute-1.amazonaws.com:5432/dcqcjnilm2ktfd'
+export AUTH0_CLIENT_ID = 'G53UYvuIkMTYBpSt2fgpFIzisFQYNso0'
+export AUTH0_CLIENT_SECRET = 'yY5mS4al1rzewSHB7h0VrSDMtnSW3F_hPO_BpWhGiidNuB_XazWc_17SU_8ybX8F'
+export AUTH0_DOMAIN = 'fsndmk.eu.auth0.com'
