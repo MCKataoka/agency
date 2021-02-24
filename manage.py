0ef45e6fc7cefcae9e6e8f1fdadd243156882fe2
MCKataoka <mario.c.kataoka@gmail.com>
2021-02-24 15:50:02 +0000

    make better

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+
 from app import create_app
-from models import db, Actor, Movie
+from models import db
 
 app = create_app()
 
@@ -9,15 +10,6 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
-
-@manager.command
-def seed():
-    Movie(title='Movie 1', release_date='2022-05-06').insert()
-    Movie(title='Movie 2', release_date='2022-05-07').insert()
-
-    Actor(name='Jason', age=22, gender='male').insert()
-    Actor(name='Janice', age=11, gender='female').insert()
 
 
 if __name__ == '__main__':
